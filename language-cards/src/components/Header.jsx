5f7cf7e5cc7a3e68a41f9b6d9ca529838bd6fbe2
Şevval Bektaş <sevvalbektas5561@gmail.com>
2022-11-01 14:49:53 +0300

    header component created

--- language-cards/src/components/Header.jsx
+++ language-cards/src/components/Header.jsx
@@ -1,8 +1,13 @@
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image'
+import reactLogo from "../assets/react.svg"
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <Container>
+        <Image src={reactLogo} width="250px"></Image>
+    </Container>
+  );
+};
 
 export default Header
