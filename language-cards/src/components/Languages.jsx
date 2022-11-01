a54b80cc1736f805b048832142dcbb21e94ae3d7
Şevval Bektaş <sevvalbektas5561@gmail.com>
2022-11-01 18:15:41 +0300

    language component started

--- language-cards/src/components/Languages.jsx
+++ language-cards/src/components/Languages.jsx
@@ -1,9 +1,15 @@
-
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 
 const Languages = ({name, img, options}) => {
+    console.log(name, img, options);
   return (
-    <div>Languages</div>
-  )
-}
+    <Container>
+        <Container>
+            <Image img={img}></Image>
+        </Container>
+    </Container>
+  );
+};
 
-export default Languages
+export default Languages;
